356d7013f7d8d1aaf094b54986c7e138481227ec
nearlynithin <nearlynithin@gmail.com>
2025-09-03 20:46:20 +0530

    feat: structs to carry scene contexts and shadowmap data

---
+++ include/game/renderContext.hpp
@@ -0,0 +1,31 @@
+#pragma once
+
+#include "raylib.h"
+#include "shader.hpp"
+
+struct SceneContext
+{
+    Camera3D lightCam;
+    Vector3 lightDir;
+    Vector4 lightColor;
+    float ambient[4];
+    Matrix lightViewProj;
+    gfx::Shader *grassShader;
+    gfx::Shader *terrainShader;
+    gfx::Shader *skyboxShader;
+    gfx::Shader *shadowMapShader;
+    gfx::Shader *cubemapShader;
+};
+
+struct shadowMapData
+{
+    RenderTexture2D shadowMap;
+    Matrix lightView;
+    Matrix lightProj;
+    Matrix lightViewProj;
+    int shadowMapSlot;
+    shadowMapData()
+      : shadowMapSlot(1)
+    {
+    }
+};
