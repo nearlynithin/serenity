6da57870c88914ceb6f75c6d0dffe918b9e6571c
nearlynithin <nearlynithin@gmail.com>
2025-09-05 11:25:28 +0530

    feat: added player modeDir and yaw to face the moving direction

--- include/game/player.hpp
+++ include/game/player.hpp
@@ -12,9 +12,12 @@ class Player
 {
   private:
     Vector3 position;
+    Vector3 moveDir;
+    float modelYaw;
     Model model;
     float speed;
     float height;
+    int animCurrentFrame;
 
   public:
     static Camera3D camera;
@@ -32,6 +35,7 @@ class Player
     static void PlayerMoves();
     static void UpdateCamera();
     static void setPlayerPosition(Vector3 pos);
+    static void UpdatePlayer();
 };
 
 #endif
--- src/player.cpp
+++ src/player.cpp
@@ -1,4 +1,5 @@
 #include "game/player.hpp"
+#include "game/resource.hpp"
 #include "raylib.h"
 #include "raymath.h"
 #include "rlgl.h"
@@ -15,8 +16,7 @@ void Player::InitPlayer()
     player.position = Vector3{0, 0, 0};
     player.height = 4.0f;
     player.speed = 5.0f;
-    Mesh mesh = GenMeshCylinder(1, player.height, 8);
-    player.model = LoadModelFromMesh(mesh);
+    player.model = ResourceManager::getInstance().getModel("player");
     Player::camera = {0};
     camera.position = {10.0f, 10.0f, 10.0f};
     camera.target = {0.0f, 0.0f, 0.0f};
@@ -28,41 +28,52 @@ void Player::InitPlayer()
     Player::yaw = 0.0f;
     Player::pitch = 0.0f;
 }
-void Player::DrawPlayer()
-{
-    Player &player = Player::getInstance();
-    DrawModelEx(player.model, player.position, Vector3{0.0f, 1.0f, 0.0f}, Player::yaw * RAD2DEG,
-                Vector3{0.4f, 0.4f, 0.4f}, RED);
-}
 
 Vector3 Player::GetPlayerPosition()
 {
     return Player::getInstance().position;
 }
 
+// Listen for inputs
 void Player::PlayerMoves()
 {
-
     Player &player = Player::getInstance();
-
-    Vector3 pos = player.GetPlayerPosition();
 
     float delta = GetFrameTime();
     Vector3 forward = {sinf(Player::yaw), 0.0f, cosf(Player::yaw)};
-
     Vector3 right = {cosf(Player::yaw), 0.0f, -sinf(Player::yaw)};
 
     forward = Vector3Normalize(forward);
     right = Vector3Normalize(right);
 
     if (IsKeyDown(KEY_W))
-        player.position = Vector3Subtract(player.position, Vector3Scale(forward, player.speed * delta));
+        player.moveDir = Vector3Subtract(player.moveDir, forward);
     if (IsKeyDown(KEY_A))
-        player.position = Vector3Subtract(player.position, Vector3Scale(right, player.speed * delta));
+        player.moveDir = Vector3Subtract(player.moveDir, right);
     if (IsKeyDown(KEY_S))
-        player.position = Vector3Add(player.position, Vector3Scale(forward, player.speed * delta));
+        player.moveDir = Vector3Add(player.moveDir, forward);
     if (IsKeyDown(KEY_D))
-        player.position = Vector3Add(player.position, Vector3Scale(right, player.speed * delta));
+        player.moveDir = Vector3Add(player.moveDir, right);
+    if (Vector3Length(player.moveDir) > 0.001f)
+    {
+        player.moveDir = Vector3Normalize(player.moveDir);
+        player.position = Vector3Add(player.position, Vector3Scale(player.moveDir, player.speed * delta));
+    }
+
+    player.speed = (IsKeyDown(KEY_LEFT_SHIFT)) ? 15.0f : 5.0f;
+}
+
+void Player::UpdatePlayer()
+{
+    if (IsKeyDown(KEY_SPACE))
+    {
+        Player &player = Player::getInstance();
+        auto rm = ResourceManager::getInstance();
+        auto &anim = rm.getModelAnimation("player");
+        player.animCurrentFrame = (player.animCurrentFrame + 1) % anim.frameCount;
+        UpdateModelAnimation(player.model, anim, player.animCurrentFrame);
+        std::cout << player.animCurrentFrame << "\n";
+    }
 }
 
 void Player::UpdateCamera()
@@ -88,13 +99,32 @@ void Player::UpdateCamera()
 
     Vector3 move = {0};
     Vector3 rot = {0.0f, 0.0f, 0.0f};
-    // UpdateCameraPro(&Player::camera, move, rot, -20.0f);
-    UpdateCameraPro(&Player::camera, move, rot, 2.0f);
+    UpdateCameraPro(&Player::camera, move, rot, 1.0f);
 }
 
 void Player::setPlayerPosition(Vector3 pos)
 {
-    // std::cout << "SET : {" << pos.x << "," << pos.y << "," << pos.z << ")\n";
     Player &player = Player::getInstance();
     player.position = pos;
+}
+
+void Player::DrawPlayer()
+{
+    Player &player = Player::getInstance();
+    if (Vector3Length(player.moveDir) > 0.001f)
+    {
+        player.modelYaw = atan2f(player.moveDir.x, player.moveDir.z) * RAD2DEG;
+    }
+
+    rlPushMatrix();
+    rlTranslatef(player.position.x, player.position.y, player.position.z);
+    rlRotatef(player.modelYaw, 0.0f, 1.0f, 0.0f);
+    rlRotatef(90.0f, 1.0f, 0.0f, 0.0f);
+    rlScalef(0.02f, 0.02f, 0.02f);
+    DrawModel(player.model, Vector3{0.0f, 0.0f, 0.0f}, 1.0f, WHITE);
+    rlPopMatrix();
+
+    player.moveDir.x = 0;
+    player.moveDir.y = 0;
+    player.moveDir.z = 0;
 }
